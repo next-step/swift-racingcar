fcaeb60e2ed9a14ef96cbe2eb077da8f9708b767
Beomkey <nkb5813@naver.com>
2021-10-27 00:14:33 +0900

    unit test init

---
+++ RacingCarTest/RacingCarTest.swift
@@ -0,0 +1,21 @@
+//
+//  RacingCarTest.swift
+//  RacingCarTest
+//
+//  Created by 남기범 on 2021/10/27.
+//
+
+import XCTest
+@testable import RacingCar
+
+class RacingCarTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+}
