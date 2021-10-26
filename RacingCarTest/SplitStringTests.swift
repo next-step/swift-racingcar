857f3fccebc1bd78a49d7fcb03c1354651472577
Mephrine <dthwrld@gmail.com>
2021-10-26 21:21:01 +0900

    Fix: 요구사항1 - 2번째 테스트 케이스 작성 추가

--- RacingCarTest/SplitStringTests.swift
+++ RacingCarTest/SplitStringTests.swift
@@ -8,12 +8,21 @@
 import XCTest
 
 class SplitStringTests: XCTestCase {
-	func testSplitString() {
+	func testSplitStringCase1() {
 		let results = "1,2".split(separator: ",")
 		let expect: [String.SubSequence] = ["1", "2"]
 		
 		XCTAssertEqual(results, expect)
 		XCTAssert(results == expect)
+		XCTAssert(results.contains("1"))
+	}
+	
+	func testSplitStringCase2() {
+		let results = "1".split(separator: ",")
+		let expect: [String.SubSequence] = ["1"]
+		
+		XCTAssertEqual(results, expect)
+		XCTAssert(results.count == 1)
 	}
 	
 	func testRemoveParentheses() {
