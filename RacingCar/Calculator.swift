927f9e27cc3be8f0afb002e1abed3c94465a7c3c
김효성 <devvenusk@gmail.com>
2021-10-27 21:56:55 +0900

    Revert "Revert "Implementation of the test teaser program""
    This reverts commit 7910dd1c968fd0fcb7c466ecfcf3fd7628a03eb9.

---
+++ RacingCar/Calculator.swift
@@ -0,0 +1,16 @@
+//
+//  Calculator.swift
+//  RacingCar
+//
+//  Created by 김효성 on 2021/10/27.
+//
+
+struct Calculator {
+    func add(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func substract(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs - rhs
+    }
+}
