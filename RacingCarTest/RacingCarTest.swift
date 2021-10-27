6fb2104b60c248cd5812b18e5e81bf7b0108f8c5
sangsun <hiddenparts@gmail.com>
2021-10-28 00:55:57 +0900

    사용하지않아 불필요한 함수 삭제

--- RacingCarTest/RacingCarTest.swift
+++ RacingCarTest/RacingCarTest.swift
@@ -9,26 +9,6 @@ import XCTest
 
 class RacingCarTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testRequire1() throws {
         XCTAssertEqual("1,2".split(separator: ","), ["1","2"])
         XCTAssertEqual("1".split(separator: ","), ["1"])
