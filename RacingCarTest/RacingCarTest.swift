d9f389ba4cc8ad45a655b583647ff860895ff399
sangsun <hiddenparts@gmail.com>
2021-10-28 01:12:29 +0900

    함수명을 좀 더 명확하게 하고 테스트케이스 구현방식 수정

--- RacingCarTest/RacingCarTest.swift
+++ RacingCarTest/RacingCarTest.swift
@@ -9,13 +9,25 @@ import XCTest
 
 class RacingCarTest: XCTestCase {
 
-    func testRequire1() throws {
-        XCTAssertEqual("1,2".split(separator: ","), ["1","2"])
-        XCTAssertEqual("1".split(separator: ","), ["1"])
+    func test_요구사항1_split을_통한_분리_1() throws {
+        let input = "1,2".split(separator: ",")
+        let expected: [Substring] = ["1","2"]
+        
+        XCTAssertEqual(input, expected)
     }
     
-    func testRequire2() throws {
-        XCTAssertEqual("(1,2)".replacingOccurrences(of: "(", with: "").replacingOccurrences(of: ")", with: ""), "1,2")
+    func test_요구사항1_split을_통한_분리_2() throws {
+        let input = "1,".split(separator: ",")
+        let expected: [Substring] = ["1"]
+        
+        XCTAssertEqual(input, expected)
+    }
+    
+    func test_요구사항2_replacingOccurrences를_활용한_분리() throws {
+        let input: String = "(1,2)".replacingOccurrences(of: "(", with: "").replacingOccurrences(of: ")", with: "")
+        let expected: String = "1,2"
+        
+        XCTAssertEqual(input, expected)
     }
 
 }
