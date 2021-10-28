7547fa4ff3d6ea8bfdc95373e8d2c84f6d563b8d
Mephrine <dthwrld@gmail.com>
2021-10-28 22:45:00 +0900

    feat($compile): write a unit test that adds two numbers

--- RacingCarTest/CalculatorTest.swift
+++ RacingCarTest/CalculatorTest.swift
@@ -5,4 +5,16 @@
 //  Created by Mephrine on 2021/10/28.
 //
 
-import Foundation
+import XCTest
+
+class CalculatorTest: XCTestCase {
+	let mockCalculator: Calculable = Calculator()
+	
+	func test_shouldGetTheCorretResultWhenAddingTwoNumbers() throws {
+		let addedResult = mockCalculator.add("3" , "5")
+		let result = "8"
+		XCTAssertEqual(addedNumber, result)
+	}
+}
+
+
