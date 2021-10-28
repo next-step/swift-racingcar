56a603178576ca7e9bb971f7c16bf0aacffd74de
felix-mr <smr3102@gmail.com>
2021-10-28 22:17:40 +0900

    Calculator 유닛 테스트
    - add, substract, divide, multiply 유닛 테스트

--- RacingCarTest/StringCalculatorTests.swift
+++ RacingCarTest/StringCalculatorTests.swift
@@ -8,25 +8,69 @@
 import XCTest
 
 class StringCalculatorTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var calculator: Calculator!
+    
+    override func setUp() {
+        super.setUp()
+        
+        self.calculator = Calculator()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        self.calculator = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_calculator_add() {
+        // Act
+        let result = calculator.add(99, to: 1)
+        
+        // Assert
+        XCTAssertEqual(100, result)
     }
+    
+    func test_calculator_subtract() {
+        // Act
+        let result = calculator.subtract(9, from: 99)
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        // Assert
+        XCTAssertEqual(90, result)
     }
+    
+    func test_calculator_divide() {
+        // Act
+        let result = calculator.divide(6, into: 2)
+        
+        // Assert
+        XCTAssertEqual(3, result)
+    }
+    
+    func test_calculator_multiply() {
+        // Act
+        let result = calculator.multiply(2, by: 5)
+        
+        // Assert
+        XCTAssertEqual(10, result)
+    }
+}
 
+class Calculator {
+    
+    func add(_ left: Int, to right: Int) -> Int {
+        return left + right
+    }
+    
+    func subtract(_ left: Int, from right: Int) -> Int {
+        return right - left
+    }
+    
+    func divide(_ left: Int, into right: Int) -> Int {
+        return left / right
+    }
+    
+    func multiply(_ left: Int, by right: Int) -> Int {
+        return left * right
+    }
 }
