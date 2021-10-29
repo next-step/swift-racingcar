7a241e01cf4e55b5aba0f17d8a5bdc22e1ff3c8d
sangsun <hiddenparts@gmail.com>
2021-10-29 22:02:30 +0900

    [2단계] 사칙연산 수행 기능 구현
    - 더하기, 빼기, 곱하기, 나누기 기능 구현

---
+++ RacingCar/Calculator.swift
@@ -0,0 +1,28 @@
+//
+//  Calculator.swift
+//  RacingCar
+//
+//  Created by sangsun on 2021/10/29.
+//
+
+import Foundation
+
+struct Calculator {
+    
+    func add(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func substract(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func multiply(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func divide(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+}
