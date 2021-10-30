89d9b489dcd0aee446d1d1c0baa4047f5f5b98bb
Beomkey <nkb5813@naver.com>
2021-10-30 10:23:48 +0900

    step2 init

---
+++ StringCalculator/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  StringCalculator
+//
+//  Created by 남기범 on 2021/10/29.
+//
+
+import Foundation
---
+++ StringCalculatorTest/StringCalculatorTest.swift
@@ -0,0 +1,18 @@
+//
+//  StringCalculatorTest.swift
+//  StringCalculatorTest
+//
+//  Created by 남기범 on 2021/10/29.
+//
+
+import XCTest
+@testable import StringCalculator
+
+class StringCalculatorTest: XCTestCase {
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+}
+
