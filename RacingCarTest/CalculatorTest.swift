497bdbfc114b7562154ac0ec901e15d997a79377
Mephrine <dthwrld@gmail.com>
2021-10-30 23:53:09 +0900

    test: Add subtract unit tests

--- RacingCarTest/CalculatorTest.swift
+++ RacingCarTest/CalculatorTest.swift
@@ -16,6 +16,13 @@ class CalculatorTest: XCTestCase {
 		XCTAssertEqual(calculator.add(220, 31), 251)
 		XCTAssertEqual(calculator.add(514, 510), 1024)
 	}
+	
+	func test_shouldGetTheCorretResultWhenSubtractingTwoNumbers() {
+		XCTAssertEqual(calculator.subtract(5, 3), 2)
+		XCTAssertEqual(calculator.subtract(6, 13), -7)
+		XCTAssertEqual(calculator.subtract(220, 31), 189)
+		XCTAssertEqual(calculator.subtract(510, 514), -4)
+	}
 }
 
 
