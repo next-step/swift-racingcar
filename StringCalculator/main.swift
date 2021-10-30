1b5eaa538f9c83a3081c924371064743c476ca92
Beomkey <nkb5813@naver.com>
2021-10-30 10:25:27 +0900

    사칙연산을 위한 프로토콜 구현

--- StringCalculator/main.swift
+++ StringCalculator/main.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+protocol ArithmeticOperationProtocol {
+    func add(_ lhs: Int, _ rhs: Int) -> Int
+    func substract(_ lhs: Int, _ rhs: Int) -> Int
+    func divide(_ lhs: Int, _ rhs: Int) -> Int
+    func multiply(_ lhs: Int, _ rhs: Int) -> Int
+}
+
+extension ArithmeticOperationProtocol {
+    func add(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func substract(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func divide(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func multiply(_ lhs: Int, _ rhs: Int) -> Int {
+        return lhs * rhs
+    }
+}
