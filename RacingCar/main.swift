b0a93bd24057a2b5674cc9edcca473028bb4c653
Mephrine <dthwrld@gmail.com>
2021-10-30 23:55:52 +0900

    feat: Implement function subtract
    Implement the subtract function written in CalculatorTest

--- RacingCar/main.swift
+++ RacingCar/main.swift
@@ -8,10 +8,15 @@ import Foundation
 
 protocol Calculable {
 	func add(_ lhs: Int, _ rhs: Int) -> Int
+	func subtract(_ lhs: Int, _ rhs: Int) -> Int
 }
 
 struct Calculator: Calculable {
 	func add(_ lhs: Int, _ rhs: Int) -> Int {
 		return lhs + rhs
+	}
+	
+	func subtract(_ lhs: Int, _ rhs: Int) -> Int {
+		return lhs - rhs
 	}
 }
