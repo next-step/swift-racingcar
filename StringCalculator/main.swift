037988854908d351e94ea50d7db792d93b0141c1
Beomkey <nkb5813@naver.com>
2021-10-30 10:33:01 +0900

    String을 변환해서 계산하기 위한 CalculatorProtocol 구현

--- StringCalculator/main.swift
+++ StringCalculator/main.swift
@@ -50,3 +50,40 @@ enum Operation: String, CaseIterable {
         return operation
     }
 }
+
+protocol CalculatorProtocol {
+    func calculate(input: String) -> Int?
+    func calculate(_ lhs: Int, _ rhs: Int, operation: Operation) -> Int
+}
+
+extension CalculatorProtocol where Self: ArithmeticOperationProtocol {
+    func calculate(input: String) -> Int? {
+        let components = input.components(separatedBy: " ")
+        let numbers = components.compactMap { Int($0) }
+        let operators = components.filter { Int($0) == nil }
+        
+        var lhs: Int = numbers[0]
+        
+        for index in 1..<numbers.count {
+            guard let operation = Operation.factory(stringValue: operators[index-1]) else {
+                return nil
+            }
+            lhs = calculate(lhs, numbers[index], operation: operation)
+        }
+        
+        return lhs
+    }
+    
+    func calculate(_ lhs: Int, _ rhs: Int, operation: Operation) -> Int {
+        switch operation {
+        case .add:
+            return add(lhs, rhs)
+        case .substract:
+            return substract(lhs, rhs)
+        case .divide:
+            return divide(lhs, rhs)
+        case .multiply:
+            return multiply(lhs, rhs)
+        }
+    }
+}
