842cc408158d85fccf99cf9d17e3d02e7c955424
Mephrine <dthwrld@gmail.com>
2021-10-31 21:15:07 +0900

    refactor: Prevent error in 'calculate' function

--- RacingCar/main.swift
+++ RacingCar/main.swift
@@ -11,7 +11,7 @@ protocol Calculable {
 	func subtract(_ lhs: Int, _ rhs: Int) -> Int
 	func multiply(_ lhs: Int, _ rhs: Int) -> Int
 	func divide(_ lhs: Int, _ rhs: Int) -> Int
-	func calculate(_ input: String) throws -> Int
+	func calculate(_ input: String?) throws -> Int
 }
 
 enum CalculatorError: Error {
@@ -37,16 +37,20 @@ struct Calculator: Calculable {
 		return lhs / rhs
 	}
 	
-	func calculate(_ input: String) throws -> Int {
-		let expressions = input.components(separatedBy: " ")
-		guard expressions.count > 0 else { throw CalculatorError.emptyValue }
+	func calculate(_ input: String?) throws -> Int {
+		guard let expressions = input?.components(separatedBy: " "),
+					expressions.count > 0
+		else {
+			throw CalculatorError.emptyValue
+		}
 		
 		var result = try expressions.first.toInt()
-		for i in 1 ..< expressions.count {
-			if expressions[i] == "+" && expressions.count > i + 1 {
-				let operand = try expressions[i + 1].toInt()
-				result = add(result, operand)
-			}
+		for i in stride(from: 1, to: expressions.count, by: 2) {
+			guard expressions[i] == "+" else { throw CalculatorError.invalidOperator }
+			guard expressions.count > i + 1 else { throw CalculatorError.invalidOperand }
+			
+			let operand = try expressions[i + 1].toInt()
+			result = add(result, operand)
 		}
 		
 		return result
