9e8c03d636b3aa9649356a0412c4a2dd53f4cdc5
Mephrine <dthwrld@gmail.com>
2021-10-31 20:22:44 +0900

    feat: Implement a calculation function
    Implement a calculation function when the operator is addition

--- RacingCar/main.swift
+++ RacingCar/main.swift
@@ -11,6 +11,13 @@ protocol Calculable {
 	func subtract(_ lhs: Int, _ rhs: Int) -> Int
 	func multiply(_ lhs: Int, _ rhs: Int) -> Int
 	func divide(_ lhs: Int, _ rhs: Int) -> Int
+	func calculate(_ input: String) throws -> Int
+}
+
+enum CalculatorError: Error {
+	case emptyValue
+	case invalidOperand
+	case invalidOperator
 }
 
 struct Calculator: Calculable {
@@ -28,5 +35,38 @@ struct Calculator: Calculable {
 	
 	func divide(_ lhs: Int, _ rhs: Int) -> Int {
 		return lhs / rhs
+	}
+	
+	func calculate(_ input: String) throws -> Int {
+		let expressions = input.components(separatedBy: " ")
+		guard expressions.count > 0 else { throw CalculatorError.emptyValue }
+		
+		var result = try expressions.first.toInt()
+		for i in 1 ..< expressions.count {
+			if expressions[i] == "+" && expressions.count > i + 1 {
+				let operand = try expressions[i + 1].toInt()
+				result = add(result, operand)
+			}
+		}
+		
+		return result
+	}
+}
+
+extension Optional where Wrapped == String {
+	func toInt() throws -> Int {
+		guard let unwrappedString = self else {
+			throw CalculatorError.invalidOperand
+		}
+		return try unwrappedString.toInt()
+	}
+}
+
+extension String {
+	func toInt() throws -> Int {
+		guard let unwrappedInt = Int(self) else {
+			throw CalculatorError.invalidOperand
+		}
+		return unwrappedInt
 	}
 }
