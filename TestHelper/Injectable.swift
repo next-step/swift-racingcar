cee4118584a02b7a835f13f15153a9d399a14e35
Beomkey <nkb5813@naver.com>
2021-10-31 19:12:47 +0900

    error  throw를 할 수 있는 function 추가

--- TestHelper/Injectable.swift
+++ TestHelper/Injectable.swift
@@ -9,14 +9,21 @@ import Foundation
 
 protocol Injectable {
     func register<Object>(_ type: Object.Type, name: String, provider: @escaping () -> Object)
+    func registerThrows<Object>(_ type: Object.Type, name: String, provider: @escaping () throws -> Object)
     func resolve<Object>(_ type: Object.Type, name: String) -> Object?
+    func resolveThrows<Object>(_ type: Object.Type, name: String) throws -> Object?
 }
 
 class StubContainer: Injectable {
     private var container = [String: () -> Any?]()
+    private var throwsContainer = [String: () throws -> Any?]()
     
     func register<Object>(_ type: Object.Type, name: String, provider: @escaping () -> Object) {
         container[name] = provider
+    }
+    
+    func registerThrows<Object>(_ type: Object.Type, name: String, provider: @escaping () throws -> Object) {
+        throwsContainer[name] = provider
     }
     
     func resolve<Object>(_ type: Object.Type, name: String) -> Object? {
@@ -24,6 +31,13 @@ class StubContainer: Injectable {
               let object = provider()
         else { return nil }
         
+        return object as? Object
+    }
+    
+    func resolveThrows<Object>(_ type: Object.Type, name: String) throws -> Object? {
+        guard let provider = throwsContainer[name] else { return nil }
+        
+        let object = try provider()
         return object as? Object
     }
 }
