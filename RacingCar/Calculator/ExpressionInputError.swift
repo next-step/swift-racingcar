9ef797eba2775a8c271c423f7df45e6b86d24f8e
yeonduing <yeonduing@gmail.com>
2021-11-01 19:53:50 +0900

    [feat] ExpressionInputError 생성

---
+++ RacingCar/Calculator/ExpressionInputError.swift
@@ -0,0 +1,14 @@
+//
+//  ExpressionInputError.swift
+//  RacingCar
+//
+//  Created by itzel.du on 2021/10/31.
+//
+
+import Foundation
+
+enum ExpressionInputError: Error {
+    case emptyInput
+    case notOperation
+    case invalidOutput
+}
