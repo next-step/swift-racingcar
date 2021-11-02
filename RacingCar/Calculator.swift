5cd3d5b55cb92f6e703e797fece9ba06868aeeb0
Mephrine <dthwrld@gmail.com>
2021-11-02 20:13:40 +0900

    refactor: Split the role of operands and results

--- RacingCar/Calculator.swift
+++ RacingCar/Calculator.swift
@@ -27,15 +27,24 @@ struct Calculator: Calculable {
 	private func calculate(by expressions: [String]) throws -> Int {
 		var result = try expressions.first.toInt()
 		for operatorIndex in findOperatorRange(in: expressions) {
-			let RHSOperandIndex = operatorIndex + 1
-			guard expressions.count > RHSOperandIndex else { throw ValueError.invalid }
-			
-			let operand = try expressions[RHSOperandIndex].toInt()
+			let RHSOperand = try findRHSOperand(by: operatorIndex, in: expressions)
+			let operands = (lhs: result, rhs: RHSOperand)
 			let arithmeticOperator = findOperator(in: expressions[operatorIndex])
-			result = try arithmeticOperator.operate(result, operand)
+			
+			result = try arithmeticOperator.operate(operands.lhs, operands.rhs)
 		}
 		
 		return result
+	}
+	
+	private func findRHSOperand(by operatorIndex: Int, in expressions: [String]) throws -> Int {
+		let RHSOperandIndex = operatorIndex + 1
+		guard expressions.count > RHSOperandIndex,
+					let RHSOperand = try? expressions[RHSOperandIndex].toInt()
+		else {
+			throw ValueError.invalid
+		}
+		return RHSOperand
 	}
 	
 	private func findOperatorRange(in expressions: [String]) -> StrideTo<Int> {
