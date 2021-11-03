6b2870515c07b3cb767b4b080801b59d783a25bd
Mephrine <dthwrld@gmail.com>
2021-11-04 00:17:57 +0900

    test: Move forward of a RacingCar
    Test a RacingCar when number is grater than 4

---
+++ RacingCarTest/RacingCarTests.swift
@@ -0,0 +1,16 @@
+//
+//  RacingCarTests.swift
+//  RacingCarTest
+//
+//  Created by Mephrine on 2021/11/03.
+//
+
+import XCTest
+
+class RacingCarTests: XCTestCase {
+	func test_shouldMoveForwardWhenNumberisGraterThan4() {
+		let racingCar = RacingCar()
+		racingCar.move(at: 5)
+		XCTAssertEqual(racingCar.position, 1)
+	}
+}
