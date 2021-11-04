69407512f8baa9d3e881ad61e7a0c2ec4b42ba64
Mino777 <jomino7777@gmail.com>
2021-11-04 21:24:46 +0900

    feat: 테스트 케이스 작성

---
+++ RacingCarTest/Step3/RacingGameTest.swift
@@ -0,0 +1,23 @@
+//
+//  RacingGameTest.swift
+//  RacingCarTest
+//
+//  Created by 조민호 on 2021/11/04.
+//
+
+import XCTest
+
+class RacingGameTest: XCTestCase {
+
+    func test_입력된값만큼_자동차를_생성한다() {
+        
+    }
+    
+    func test_무작위값이_4_이상일경우_자동차가_이동한다() {
+        
+    }
+    
+    func test_무작위값이_4_이하일경우_자동차가_이동하지않는다() {
+        
+    }
+}
