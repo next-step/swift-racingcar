1233eb1334c9b546780deac2c7b340c706e9ecc3
Mino777 <jomino7777@gmail.com>
2021-11-05 23:20:01 +0900

    test: 테스트 코드 작성

--- RacingCarTest/Step3/RacingGameTest.swift
+++ RacingCarTest/Step3/RacingGameTest.swift
@@ -8,16 +8,33 @@
 import XCTest
 
 class RacingGameTest: XCTestCase {
-
-    func test_입력된값만큼_자동차를_생성한다() {
+    func test_입력값만큼_자동차를_생성한다() throws {
+        let result = try CarsFactory().getCars(numberOfCars: 3).count
+        let expected = 3
         
+        XCTAssertEqual(expected, result)
     }
     
-    func test_무작위값이_4_이상일경우_자동차가_이동한다() {
-        
+    func test_입력값이_음수일경우_자동차를_생산하지않는다() {
+        XCTAssertThrowsError(try CarsFactory().getCars(numberOfCars: -1)) { error in
+            XCTAssertEqual(error as! CarsFactory.CarsFactoryError, CarsFactory.CarsFactoryError.notExistCar)
+        }
     }
     
-    func test_무작위값이_4_이하일경우_자동차가_이동하지않는다() {
+    func test_position값이_4_미만일경우_움직이지않는다() {
+        let car = Car()
         
+        car.move(amount: 3)
+        
+        XCTAssertEqual(car.position, GameOption.defaultPosition)
+    }
+    
+    func test_position값이_4_이상일경우_전진한다() {
+        let car = Car()
+        
+        car.move(amount: 4)
+                
+        XCTAssertEqual(car.position, GameOption.moveDistance)
     }
 }
+
