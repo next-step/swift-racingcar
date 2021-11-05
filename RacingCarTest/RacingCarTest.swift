4168f0fb2b5a985cc45eae305e8825775ad687ef
NamKiBeom <nkb5813@naver.com>
2021-11-06 00:07:36 +0900

    자동차가 전진하는 조건에 대한 테스트

--- RacingCarTest/RacingCarTest.swift
+++ RacingCarTest/RacingCarTest.swift
@@ -9,12 +9,28 @@ import XCTest
 @testable import RacingCar
 
 class RacingCarTest: XCTestCase {
+    var viewModel: StubRacingCarViewModel!
 
     override func setUpWithError() throws {
-        
+        viewModel = StubRacingCarViewModel()
     }
 
     override func tearDownWithError() throws {
+        viewModel = nil
+    }
+    
+    func test_racing_car_forward() {
+        let expect = expectation(description: "자동차의 연료가 4이상 9 이하일 경우 앞으로 전진한다.")
+        guard let carFuel: Int = (4...9).randomElement() else { return }
         
+        viewModel.called(name: "isForward", verify: { isForward in
+            if (4...9).contains(carFuel) {
+                expect.fulfill()
+            }
+        })
+        
+        viewModel.startRacing()
+        
+        self.wait(for: [expect], timeout: 0.1)
     }
 }
---
+++ RacingCarTest/StubRacingCarViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  StubRacingCarViewModel.swift
+//  RacingCarTest
+//
+//  Created by 남기범 on 2021/11/05.
+//
+
+import Foundation
+import TestHelper
+
+class StubRacingCarViewModel: StubContainer {
+    
+}
