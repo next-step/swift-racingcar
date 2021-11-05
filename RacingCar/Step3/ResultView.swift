42711a743fd6ec423fce33fa0a1a5c1e589babde
Mino777 <jomino7777@gmail.com>
2021-11-05 23:17:43 +0900

    feat: ResultView 구현

---
+++ RacingCar/Step3/ResultView.swift
@@ -0,0 +1,21 @@
+//
+//  ResultView.swift
+//  RacingCar
+//
+//  Created by 조민호 on 2021/11/05.
+//
+
+import Foundation
+
+struct ResultView {
+    private let resultTitle = "실행 결과"
+    
+    func printResultTitle() {
+        print("")
+        print(resultTitle)
+    }
+    
+    func printResult(result: String) {
+        print(result)
+    }
+}
