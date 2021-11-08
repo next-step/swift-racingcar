961d7049a108e9451c1a2cf9195a0b180c6bda4e
yeonduing <yeonduing@gmail.com>
2021-11-08 23:33:12 +0900

    [feat] RacingCarGame 생성
    - randomRange에 따라 RacingCarMover를 이용해 레이싱카를 이동시키는 객체

--- RacingCar/RacingCar.swift
+++ RacingCar/RacingCar.swift
@@ -51,3 +51,24 @@ struct RacingCarMover {
         }
     }
 }
+
+struct RacingCarGame {
+    let randomRange: Range<Int>
+    private var racingCarMovers: [RacingCarMover]
+    
+    init(randomRange: Range<Int>, racingCarMovers: [RacingCarMover]) {
+        self.randomRange = randomRange
+        self.racingCarMovers = racingCarMovers
+    }
+    
+    var racingCarPositions: [Position] {
+        racingCarMovers.map { $0.racingCarPosition }
+    }
+    
+    mutating func tryMovingRacingCarsWithRandomNumbers() {
+        racingCarMovers.indices.forEach {
+            racingCarMovers[$0].tryMoving(with: Int.random(in: randomRange))
+        }
+    }
+}
+
