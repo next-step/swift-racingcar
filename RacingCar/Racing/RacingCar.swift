e72d1c8519d2d1aee523c42e689769b4e9f81285
Mephrine <dthwrld@gmail.com>
2021-11-08 22:49:10 +0900

    refactor: Add name of RacingCar

--- RacingCar/Racing/RacingCar.swift
+++ RacingCar/Racing/RacingCar.swift
@@ -25,6 +25,11 @@ struct Position {
 
 final class RacingCar {
 	private(set) var position: Position = Position(position: RacingOption.defaultRacingCarposition, range: RacingOption.movementRange)
+	let name: String
+	
+	init(name: String) {
+		self.name = name
+	}
 	
 	func move(at score: Int) {
 		position.increase(at: score)
