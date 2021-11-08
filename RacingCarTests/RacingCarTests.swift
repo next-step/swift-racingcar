e31f040e49cd45cd4f28bd2ed53ab62908be500a
yeonduing <yeonduing@gmail.com>
2021-11-08 20:47:44 +0900

    [test] RacingCarMover 테스트

--- RacingCarTests/RacingCarTests.swift
+++ RacingCarTests/RacingCarTests.swift
@@ -37,4 +37,33 @@ class RacingCarTests: XCTestCase {
         
         XCTAssertFalse(moveChecker.isMovable(0))
     }
+    
+    func test_RacingCarMover_move() {
+        var position = Position(position: 0, distanceUnit: 1)
+        let racingCar = RacingCar(position: position)
+        let moveChecker = MoveChecker(movableRange: Range<Int>(4...9))
+        var racingCarMover = RacingCarMover(
+            racingCar: racingCar,
+            moveChecker: moveChecker
+        )
+        
+        racingCarMover.moveIfPossible(with: 4)
+        position.move()
+        
+        XCTAssertEqual(racingCarMover.racingCarPosition, position)
+    }
+    
+    func test_RacingCarMover_not_move() {
+        let position = Position(position: 0, distanceUnit: 1)
+        let racingCar = RacingCar(position: position)
+        let moveChecker = MoveChecker(movableRange: Range<Int>(4...9))
+        var racingCarMover = RacingCarMover(
+            racingCar: racingCar,
+            moveChecker: moveChecker
+        )
+        
+        racingCarMover.moveIfPossible(with: 1)
+        
+        XCTAssertEqual(racingCarMover.racingCarPosition, position)
+    }
 }
