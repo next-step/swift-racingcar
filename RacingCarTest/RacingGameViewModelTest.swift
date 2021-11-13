3c55dd81f35f2a71cf6776f6f3c902b0da1b82de
BeomKey <nkb5813@naver.com>
2021-11-13 21:08:50 +0900

    우승자 선정 로직 테스트케이스 작성

--- RacingCarTest/RacingGameViewModelTest.swift
+++ RacingCarTest/RacingGameViewModelTest.swift
@@ -67,4 +67,25 @@ extension RacingGameViewModelTest {
         
         wait(for: [expect], timeout: 0.1)
     }
+    
+    func test_select_racing_winners() {
+        let cars = [
+            RacingCar(name: "bmw1.0"),
+            RacingCar(name: "bmw2.0"),
+            RacingCar(name: "bmw3.0"),
+            RacingCar(name: "bmw4.0")
+        ]
+        
+        viewModel = RacingGameViewModel(cars: cars)
+        cars[0].attemptForward(4)
+        cars[0].attemptForward(5)
+        cars[1].attemptForward(5)
+        cars[1].attemptForward(6)
+        let winners = viewModel.winners
+        
+        XCTAssertTrue(winners.contains(cars[0].name))
+        XCTAssertTrue(winners.contains(cars[1].name))
+        XCTAssertFalse(winners.contains(cars[2].name))
+        XCTAssertFalse(winners.contains(cars[3].name))
+    }
 }
