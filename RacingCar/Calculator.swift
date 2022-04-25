17c9ad350551f3c015128dc75103f22aaa558ef2
mansa.kim <zombietux@naver.com>
2022-04-26 07:32:57 +0900

    feat: stack/count 프로퍼티 선언 후 calculate 메서드 추가
    stack의 용도는 연산식 배열에서 하나씩 계산하기 위한 용도이며, count는 1 + 2 처럼 한번만 계산하고 끝나는게 아니기 떄문에 연산자의 숫자 만큼 계산해줘야 한다는 플래그 값으로 생성해 둠

--- RacingCar/Calculator.swift
+++ RacingCar/Calculator.swift
@@ -9,13 +9,41 @@ import Foundation
 
 class Calculator: Calculable {
     private var number: Int
+    private var stack: [String]
+    private var count: Int
     
-    init(number: Int = 0) {
+    enum CalcOperator {
+        case add, subtract, multiply, divide, none
+    }
+    
+    init(number: Int = 0, stack: [String] = [], count: Int = 0) {
         self.number = number
+        self.stack = stack
+        self.count = count
+    }
+    
+    func calculate(expression: String) {
+        self.stack = self.splitCalcExpression(expression)
+        
+        let leftNumber = Int(stack.removeFirst()) ?? 0
+        let `operator` = self.generateCalcOperator(stringOperator: stack.removeFirst())
+        let rightNumber = Int(stack.removeFirst()) ?? 0
+        
+        switch `operator` {
+        case .add: self.add(left: leftNumber, right: rightNumber)
+        case .subtract: self.subtract(left: leftNumber, right: rightNumber)
+        case .multiply: self.multiply(left: leftNumber, right: rightNumber)
+        case .divide: self.divide(left: leftNumber, right: rightNumber)
+        case .none: break
+        }
     }
     
     func calculated() -> Int {
         self.number
+    }
+    
+    func reset() {
+        self.number = 0
     }
     
     func add(left leftNumber: Int, right rightNumber: Int) {
