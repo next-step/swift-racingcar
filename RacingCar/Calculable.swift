b521da86c21829550d591b48f616393ae1bb0255
mansa.kim <zombietux@naver.com>
2022-04-25 20:47:16 +0900

    feat: Calculable 프로토콜 추가

---
+++ RacingCar/Calculable.swift
@@ -0,0 +1,14 @@
+//
+//  Calculable.swift
+//  RacingCar
+//
+//  Created by 카카오VX on 2022/04/25.
+//
+
+protocol Calculable {
+    func calculated() -> Int
+    func add(left leftNumber: Int, right rightNumber: Int)
+    func subtract(left leftNumber: Int, right rightNumber: Int)
+    func multiply(left leftNumber: Int, right rightNumber: Int)
+    func divide(left leftNumber: Int, right rightNumber: Int)
+}
