8e78a12e42d1a5291a94d022e86d85daa9073b53
mansa.kim <zombietux@naver.com>
2022-04-25 19:20:43 +0900

    feat: 요구사항2 특수문자 제거 테스트 추가
    components / replacingOccurrences를 사용하여 특수문자 제거

--- RacingCarTest/RacingCarTest.swift
+++ RacingCarTest/RacingCarTest.swift
@@ -20,4 +20,24 @@ class RacingCarTest: XCTestCase {
         measure {
         }
     }
+    
+    func testSplitString() {
+        let splitedArray = splitString("1,2")
+        
+        XCTAssertEqual(splitedArray[0], "1")
+        XCTAssertEqual(splitedArray[1], "2")
+    }
+    
+    func testSplitWithoutSeparator() {
+        let splitedArray = splitString("1")
+        
+        XCTAssertEqual(splitedArray[0], "1")
+    }
+}
+
+private extension RacingCarTest {
+    func splitString(_ target: String) -> [String] {
+        let stringWithBracketRemoved = target.replacingOccurrences(of: "()", with: "")
+        return stringWithBracketRemoved.components(separatedBy: ",")
+    }
 }
