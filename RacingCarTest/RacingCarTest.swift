e46b4c6d0709755b238b153dc68988bbef4948b5
mansa.kim <zombietux@naver.com>
2022-04-25 17:49:23 +0900

    feat: RacingCarText 파일 생성
    테스트 파일 생성과 사용하지 않는 주석 삭제

---
+++ RacingCarTest/RacingCarTest.swift
@@ -0,0 +1,23 @@
+//
+//  RacingCarTest.swift
+//  RacingCarTest
+//
+//  Created by 카카오VX on 2022/04/25.
+//
+
+import XCTest
+
+class RacingCarTest: XCTestCase {
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        measure {
+        }
+    }
+}
