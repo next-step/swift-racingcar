923b88cb365863d455fdde772f9d8738481e169b
mansa.kim <zombietux@naver.com>
2022-04-25 20:17:09 +0900

    feat: step2 calculator 개체 추가 후 사칙연산 메서드 추가
    반환형 메서드로 만든게 아닌 개체 안에 프로퍼티를 선언했는데 메서드를 테스트할 때 반황형으로 만드는게 더 나을지에 대한 고민이 됨.

---
+++ RacingCar/Calculator.swift
@@ -0,0 +1,32 @@
+//
+//  Calculator.swift
+//  RacingCar
+//
+//  Created by 카카오VX on 2022/04/25.
+//
+
+import Foundation
+
+class Calculator {
+    private var number: Int
+    
+    init(number: Int = 0) {
+        self.number = number
+    }
+    
+    func add(left leftNumber: Int, right rightNumber: Int) {
+        self.number = leftNumber+rightNumber
+    }
+    
+    func subtract(left leftNumber: Int, right rightNumber: Int) {
+        self.number = leftNumber-rightNumber
+    }
+    
+    func multiply(left leftNumber: Int, right rightNumber: Int) {
+        self.number = leftNumber*rightNumber
+    }
+    
+    func divide(left leftNumber: Int, right rightNumber: Int) {
+        self.number = leftNumber/rightNumber
+    }
+}
