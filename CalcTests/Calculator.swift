34f06c5ee7f09d10aaa88ffb305de65fcf4a34f2
YuchanSong <dvlpr_uchan@kakao.com>
2022-04-26 21:30:38 +0900

    Update Calculator.swift
    계산기 연산자 enum 추가

--- CalcTests/Calculator.swift
+++ CalcTests/Calculator.swift
@@ -5,6 +5,13 @@
 //  Created by ycsong on 2022/04/26.
 //
 
+enum CalcOperation: String {
+    case PLUS = "+"
+    case MINUS = "-"
+    case MULTIPLIED = "*"
+    case DIVIDED = "/"
+}
+
 struct Calculator {
     func add(_ lhs: Int, _ rhs: Int) -> Int {
         return lhs + rhs
