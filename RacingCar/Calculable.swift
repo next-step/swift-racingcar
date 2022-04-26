88122771ee824815cefcc0dbcce9f5a92e5de2fb
mansa.kim <zombietux@naver.com>
2022-04-26 17:02:40 +0900

    feat: Calculable 프로토콜 수정

--- RacingCar/Calculable.swift
+++ RacingCar/Calculable.swift
@@ -6,11 +6,13 @@
 //
 
 protocol Calculable {
-    func calculate(expression: String)
+    func execute(expression: String)
+    func calculate(left leftNumber: Int, right rightNumber: Int, operator: Calculator.CalcOperator)
     func calculated() -> Int
     func reset()
     func add(left leftNumber: Int, right rightNumber: Int)
     func subtract(left leftNumber: Int, right rightNumber: Int)
     func multiply(left leftNumber: Int, right rightNumber: Int)
     func divide(left leftNumber: Int, right rightNumber: Int)
+    func splitCalcExpression(_ expression: String) -> (stack: [String], count: Int)
 }
