7249519e9af8bb01ecc5850347019144cdc81cf1
mansa.kim <zombietux@naver.com>
2022-04-27 22:56:39 +0900

    feat: RacingInfo 개체 개발
    Racing 개체에서 만들어진 결과를 받아 ResultView에 알려주는 RacingInfo 개체 개발

---
+++ RacingCar/RacingInfo.swift
@@ -0,0 +1,20 @@
+//
+//  RacingInfo.swift
+//  RacingCar
+//
+//  Created by 카카오VX on 2022/04/27.
+//
+
+import Foundation
+
+struct RacingInfo: RacingResultPrintable {
+    private var racing: [[Int]]
+    
+    init(racing: [[Int]]) {
+        self.racing = racing
+    }
+    
+    func result() -> [[Int]] {
+        racing
+    }
+}
