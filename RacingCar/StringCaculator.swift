8d88f82e49bfeed0c02e3de4849c3adf7d0dc181
지노지노_정진호 <zino@afreecatv.com>
2022-04-28 12:59:07 +0900

    feture: 문자열 계산기에 요구사항에 따른 기능메서드 구현

--- RacingCar/StringCaculator.swift
+++ RacingCar/StringCaculator.swift
@@ -10,11 +10,86 @@ import Foundation
 
 class StringCaculator {
     
-    func splitString(to string: String, separator: Character) -> [String] {
-        return string
-                .components(separatedBy: ["(", ")"])
-                .joined()
-                .split(separator: separator).map { String($0)}
+    enum StringCaculatorErorType: Error {
+        case inputError
+        case divideByZero
+        case notComputationSymbol
+    }
+    
+    enum OperatorType: String {
+        case plus = "+"
+        case minus = "-"
+        case multiply = "*"
+        case divide = "/"
+    }
+    
+    // 덧셈
+    func plus(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    // 뺄셈
+    func minus(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    // 곱셈
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    // 나눗셈
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    // 입력값 nil 체크
+    func validateNilCheckFor(input: String)  throws -> String {
+        guard !input.isEmpty else {
+            throw StringCaculatorErorType.inputError
+        }
+        return input
+    }
+    
+    
+    // 사친연산
+    func caculate(lhs: Int, rhs: Int, operator: OperatorType) throws -> Int {
+        switch `operator` {
+        case .plus:
+           return plus(lhs: lhs, rhs: rhs)
+        case .minus:
+            return minus(lhs: lhs, rhs: rhs)
+        case .multiply:
+            return multiply(lhs: lhs, rhs: rhs)
+        case .divide:
+            try divideOperandZero(operand: rhs)
+            return divide(lhs: lhs, rhs: rhs)
+        }
+    }
+    
+    //0으로 나누는 경우 체크
+    func divideOperandZero(operand: Int) throws {
+        if operand == 0 {
+            throw StringCaculatorErorType.divideByZero
+        }
+    }
+    
+    // 공백문자제거
+    func removeSpacingCharacter(input: String) -> [String] {
+        return input.components(separatedBy: " ")
+    }
+    
+    //들어온 문자가 연산자인지 체크
+    func isOperator(operand: String) throws -> Bool {
+        guard (OperatorType(rawValue: operand) != nil) else {
+            throw StringCaculatorErorType.notComputationSymbol
+        }
+        return true
+    }
+    
+    //들어온 문자가 피연산자인지 체크
+    func isOperand(input: String) -> Bool {
+        return (Int(input) != nil)
     }
 }
 
