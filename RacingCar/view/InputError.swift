e8ea5a41de8f70dda99bf23492c0959841b8cdeb
YuchanSong <dvlpr_uchan@kakao.com>
2022-04-28 17:38:44 +0900

    입력 오류에 대한 에러 추가

--- RacingCar/view/InputError.swift
+++ RacingCar/view/InputError.swift
@@ -5,6 +5,13 @@
 //  Created by ycsong on 2022/04/28.
 //
 
-enum InputEror: Error {
+enum InputEror: Error, CustomDebugStringConvertible {
     case invalidInput
+    
+    var debugDescription: String {
+        switch self {
+        case .invalidInput:
+            return "사용자 입력 오류"
+        }
+    }
 }
