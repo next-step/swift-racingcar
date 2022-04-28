958817272db777b86c7adfd6cda04db3b90fd439
YuchanSong <dvlpr_uchan@kakao.com>
2022-04-28 17:38:31 +0900

    자동차 클래스 생성

---
+++ RacingCar/view/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  RacingCar
+//
+//  Created by ycsong on 2022/04/28.
+//
+
+class Car {
+    public var point: Int = 0
+    
+    init() { }
+    
+    func foward() {
+        self.point += 1
+    }
+}
