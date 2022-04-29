eec9353b5ddee92df01902e052b2614cb2da7283
이정배 <jeongbae1212@gmail.com>
2022-04-30 04:23:48 +0900

    refactor: string split 테스트 분리
    step 1 피드백 반영

--- RacingCarTest/Step1/StringSplitTest.swift
+++ RacingCarTest/Step1/StringSplitTest.swift
@@ -10,10 +10,12 @@ import XCTest
 
 class StringSplitTest: XCTestCase {
 
-    func testStringSplitRequirement1() {
+    func testStringSplitMultiple() {
         XCTAssertEqual("1,2".split(separator: ","), ["1", "2"])
         XCTAssert("1,2".split(separator: ",") == ["1", "2"])
-        
+    }
+    
+    func testSplitStringSingle() {
         XCTAssertEqual("1".split(separator: ","), ["1"])
         XCTAssert("1".split(separator: ",") == ["1"])
     }
