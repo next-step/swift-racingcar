94d6d40a20d78f343b8aa82b3ace97ba079866b6
이정배 <jeongbae1212@gmail.com>
2022-04-30 05:06:30 +0900

    refactor: SetTest 보완
    setCount()의 테스트 시나리오 보완
    testsetUpWithError()에서 불필요한 코드 제거

--- RacingCarTest/Step1/SetTest.swift
+++ RacingCarTest/Step1/SetTest.swift
@@ -29,6 +29,8 @@ class SetTests: XCTestCase {
         // "1" 주입
         numbers.insert(1)
         XCTAssertEqual(numbers.count, 3, "1 주입 이후 Set의 현재 개수는 3이어야 한다.")
+        // 현재 "1"을 가지고 있는지 확인
+        XCTAssertTrue(numbers.contains(1), "1이 있어야 하는 상황이다.")
         // "1" 주입
         numbers.insert(1)
         // 한번 더 넣어도 갯수는 같아야 한다. Set은 같은 값은 하나만 가질 수 있기 때문이다.
@@ -43,25 +45,7 @@ class SetTests: XCTestCase {
     
     // 새로운 테스트가 추가되어도 setUpWithError에 의해서 다시 값이 설정되어야 한다.
     func testsetUpWithError() {
-        // testCount()와 내용이 동일하다.
-
         // setUp 이후 현재 갯수는 3개여야 한다.
         XCTAssertEqual(numbers.count, 3)
-        // 1개 제거 후 테스트
-        numbers.remove(1)
-        XCTAssertEqual(numbers.count, 2)
-        // "1" 주입
-        numbers.insert(1)
-        XCTAssertEqual(numbers.count, 3)
-        // "1" 주입
-        numbers.insert(1)
-        // 한번 더 넣어도 갯수는 같아야 한다. Set은 같은 값은 하나만 가질 수 있기 때문이다.
-        XCTAssertEqual(numbers.count, 3)
-        // 새로운 값 주입
-        numbers.insert(7)
-        XCTAssertEqual(numbers.count, 4)
-        // 다 없앤다.
-        numbers.removeAll()
-        XCTAssertEqual(numbers.count, 0)
     }
 }
