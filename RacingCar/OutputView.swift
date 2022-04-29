879382acc4838b6f2da2ad400b48e12ccf2f85aa
RoadtoS7 <kshvv975@gmail.com>
2022-04-29 21:21:30 +0900

    feat(OutputView): 결과값 출력을 담당하는 OutputView 구현

---
+++ RacingCar/OutputView.swift
@@ -0,0 +1,14 @@
+//
+//  OutputView.swift
+//  RacingCar
+//
+//  Created by nylah.j on 2022/04/29.
+//
+
+import Foundation
+
+struct OutputView {
+    static func show(output: Int) {
+        print("결과값: \(output)")
+    }
+}
