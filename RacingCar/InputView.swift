3599a1b93219d0389d2446d9fdff0aca7a7c32b6
RoadtoS7 <kshvv975@gmail.com>
2022-04-29 21:25:59 +0900

    refactor(InputView): input method를 static 으로 변경
    - 변경한 이유: util 기능만 하는 메서드이기 때문이다

--- RacingCar/InputView.swift
+++ RacingCar/InputView.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 struct InputView {
-    func input() -> String? {
+    static func input() -> String? {
         print("연산식을 입력하세요.")
         print("(연산자 우선순위에 관계없이 왼쪽에서 오른쪽으로 연산이 진행됩니다.)")
         return readLine()
