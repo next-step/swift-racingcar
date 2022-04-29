f5f1f64b40e848af7aecd12b8c83d8b4faebc63c
mansa.kim <zombietux@naver.com>
2022-04-29 18:49:02 +0900

    feat: 레이싱플레이어 개체 추가 -> AbleToRace 프로토콜 채택

---
+++ RacingCar/RacingPlayer.swift
@@ -0,0 +1,36 @@
+//
+//  RacingPlayer.swift
+//  RacingCar
+//
+//  Created by 카카오VX on 2022/04/29.
+//
+
+import Foundation
+
+protocol AbleToRace {
+    func racerName() -> String
+    func moveCountByRacer() -> Int
+    mutating func move(foward: Int)
+}
+
+struct RacingPlayer: Equatable, AbleToRace {
+    private var name: String
+    private var moveCount: Int
+    
+    init(name: String, moveCount: Int = 0) {
+        self.name = name
+        self.moveCount = moveCount
+    }
+    
+    mutating func move(foward: Int) {
+        moveCount += foward
+    }
+    
+    func racerName() -> String {
+        name
+    }
+    
+    func moveCountByRacer() -> Int {
+        moveCount
+    }
+}
