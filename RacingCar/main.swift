bbc0de346cac75a6837aae91ada72a0e65f59829
RoadtoS7 <kshvv975@gmail.com>
2022-04-29 21:30:37 +0900

    feat(main): 계산기 application 메인 로직 구현

--- RacingCar/main.swift
+++ RacingCar/main.swift
@@ -1,8 +1,12 @@
 //
 //  RacingCar - main.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import Foundation
 
+let input = InputView.input()
+let (operations, operands) = try InputParser.parse(input)
+let result = try Calculator().calculate(operands: operands, operators: operations)
+OutputView.show(output: result)
