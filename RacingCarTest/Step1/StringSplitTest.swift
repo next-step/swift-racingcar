c86a29d1898895e5d10b92e066c0c14fa12c3778
이정배 <jeongbae1212@gmail.com>
2022-04-30 04:54:46 +0900

    docs: 주석 추가
    step1 보강

--- RacingCarTest/Step1/StringSplitTest.swift
+++ RacingCarTest/Step1/StringSplitTest.swift
@@ -10,16 +10,19 @@ import XCTest
 
 class StringSplitTest: XCTestCase {
 
+    /// 요구사항 1.1
     func testStringSplitMultiple() {
         XCTAssertEqual("1,2".split(separator: ","), ["1", "2"])
         XCTAssert("1,2".split(separator: ",") == ["1", "2"])
     }
     
+    /// 요구사항 1.2
     func testSplitStringSingle() {
         XCTAssertEqual("1".split(separator: ","), ["1"])
         XCTAssert("1".split(separator: ",") == ["1"])
     }
     
+    /// 요구사항 2
     func testSplitStringWithRemovingBraces() {
         XCTAssertEqual("(1,2)".replacingOccurrences(of: "(", with: "")
             .replacingOccurrences(of: ")", with: "")
