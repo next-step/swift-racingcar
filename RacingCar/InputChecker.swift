887f90b79184aea04c48295bf9518453321e41e2
RoadtoS7 <kshvv975@gmail.com>
2022-04-30 19:37:22 +0900

    feat: 사용자의 입력이 유효한지 검사하는 InputChecker 구현
    자동차 대수와 시도할 횟수는 양의 정수여야 함을 검사한다.

---
+++ RacingCar/InputChecker.swift
@@ -0,0 +1,18 @@
+//
+//  InputChecker.swift
+//  RacingCar
+//
+//  Created by nylah.j on 2022/04/30.
+//
+
+import Foundation
+
+struct InputChecker {
+    static func isValid(carCount: Int?) -> Bool {
+        return carCount != nil && carCount! > 0
+    }
+    
+    static func isValid(roundCount: Int?) -> Bool {
+        return roundCount != nil && roundCount! > 0
+    }
+}
