afbcb84a4831182b3cc36ff93fe6db53b869b881
sujin <rkdthd1234@naver.com>
2022-05-01 00:41:07 +0900

    4단계. RecordHistoryTests 추가

--- RacingCarTest/RacingCar/RecordHistoryTests.swift
+++ RacingCarTest/RacingCar/RecordHistoryTests.swift
@@ -21,7 +21,7 @@ class RoundHistoryTests: XCTestCase {
     
     func test_roundCount_whenAppendOneRound_equalToOne() throws {
         // given
-        let car: Car = Car(name: "sujin", position: 0)
+        let car: Car = Car(name: "naljin", position: 0)
         let round: Round = Round(cars: [car])
         
         // when
@@ -31,5 +31,60 @@ class RoundHistoryTests: XCTestCase {
         let result = sut.rounds.count
         let expectation = 1
         XCTAssertEqual(result, expectation)
+    }
+    
+    func test_appendedCarInfo() throws {
+        // given
+        let car: Car = Car(name: "naljin",
+                           position: 0)
+        let round: Round = Round(cars: [car])
+        
+        // when
+        sut.append(round)
+        
+        // then
+        let firstRound = sut.rounds.first
+        let firstCar = firstRound?.cars.first
+        XCTAssertEqual(firstCar?.name, "naljin")
+        XCTAssertEqual(firstCar?.position, 0)
+    }
+    
+    func test_winningCarNames_whenHasNoAppendedRound_toBempty() throws {
+        // then
+        XCTAssertTrue(sut.winningCars.isEmpty)
+    }
+    
+    func test_winningCarNames_whenHasNoSameMaxPosition() throws {
+        // given
+        let firstCar = Car(name: "car1", position: 1)
+        let secondCar = Car(name: "car2", position: 2)
+        let thirdCar = Car(name: "car3", position: 3)
+        
+        let finalRound: Round = Round(cars: [firstCar, secondCar, thirdCar])
+        
+        // when
+        sut.append(finalRound)
+        
+        // then
+        let winningCarNames = sut.winningCars.map { $0.name }
+        let expectation = ["car3"]
+        XCTAssertEqual(winningCarNames, expectation)
+    }
+    
+    func test_winningCarNames_whenHasSameMaxPosition_returnsBoth() throws {
+        // given
+        let firstCar = Car(name: "car1", position: 1)
+        let secondCar = Car(name: "car2", position: 2)
+        let thirdCar = Car(name: "car3", position: 2)
+        
+        let finalRound: Round = Round(cars: [firstCar, secondCar, thirdCar])
+        
+        // when
+        sut.append(finalRound)
+        
+        // then
+        let winningCarNames = sut.winningCars.map { $0.name }
+        let expectation = ["car2", "car3"]
+        XCTAssertEqual(winningCarNames, expectation)
     }
 }
