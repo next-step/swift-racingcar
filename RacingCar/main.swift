67bc38ecd0ef5684ff60c175a600bcbb495966ad
Channy <dvlpr_uchan@kakao.com>
2022-04-30 23:49:36 +0900

    step4. main 클래스 리팩토링

--- RacingCar/main.swift
+++ RacingCar/main.swift
@@ -7,7 +7,6 @@
 do {
     print("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).")
     let carInfo = try InputView.readCarInfo()
-    print(carInfo)
     
     print("시도할 횟수는 몇 회인가요? ")
     let gameCount = try InputView.readCount()
