68806bd74e978175b323a2071b66356611c16d73
Channy <dvlpr_uchan@kakao.com>
2022-04-30 23:08:49 +0900

    step4. 승자 출력 메서드 리팩토링

--- RacingCar/view/resultViews/ResultView.swift
+++ RacingCar/view/resultViews/ResultView.swift
@@ -14,7 +14,7 @@ class ResultView {
     
     static func printWinner(_ cars: [Car]) {
         let maxPoint: Int? = cars.map { $0.point }.max()
-        let winners = cars.filter { $0.point == maxPoint }
+        let winners: [String] = cars.filter { $0.point == maxPoint ?? 0 }
             .map { $0.name }
         
         print("\(winners.joined(separator: ", "))가 최종 우승했습니다.")
