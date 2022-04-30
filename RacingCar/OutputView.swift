cbb0ba63e161fbbe62be6cc25ee971832fe39f06
RoadtoS7 <kshvv975@gmail.com>
2022-04-30 19:29:39 +0900

    feat: 게임 결과를 출력하는 OutputView 구현

---
+++ RacingCar/OutputView.swift
@@ -0,0 +1,15 @@
+//
+//  OutputView.swift
+//  RacingCar
+//
+//  Created by nylah.j on 2022/04/30.
+//
+
+import Foundation
+
+struct OutputView {
+    static func show(formattedGameResult: String) {
+        print("실행 결과")
+        print(formattedGameResult)
+    }
+}
