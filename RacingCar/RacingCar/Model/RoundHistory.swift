13f7fc2ac66df1c77cefcfb79638b3ec674251fc
sujin <rkdthd1234@naver.com>
2022-05-01 00:38:38 +0900

    4단계. 최종 결과에서 승리한 Car 반환하는 winningCars 프로퍼티 추가

--- RacingCar/RacingCar/Model/RoundHistory.swift
+++ RacingCar/RacingCar/Model/RoundHistory.swift
@@ -10,6 +10,25 @@ import Foundation
 final class RoundHistory {
     
     private(set) var rounds: [Round] = []
+    var winningCars: [Car] {
+        guard let finalRound = rounds.last else {
+            return []
+        }
+        
+        let maxPositionInFinalRound = finalRound
+            .cars
+            .map { car in
+                car.position
+            }
+            .max()
+        
+        let winningCars = finalRound
+            .cars
+            .filter { car in
+                car.position == maxPositionInFinalRound
+            }
+        return winningCars
+    }
     
     func append(_ round: Round) {
         rounds.append(round)
