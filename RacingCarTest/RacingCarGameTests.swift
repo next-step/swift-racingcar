a5ce61370e6d3271f412f0875add65511f88ba0b
sujin <rkdthd1234@naver.com>
2022-04-30 22:48:02 +0900

    3단계. RacingCarGame 예외 상황 테스트 케이스 추가

--- RacingCarTest/RacingCarGameTests.swift
+++ RacingCarTest/RacingCarGameTests.swift
@@ -9,29 +9,24 @@ import XCTest
 
 class RacingCarGameTests: XCTestCase {
 
+    var randomDigitNumberMaker: RandomDigitNumberMakerStub!
     var sut: RacingCarGame!
-    var carCount: Int!
-    var raceCount: Int!
     
     override func setUpWithError() throws {
-        carCount = 4
-        raceCount = 2
-        
         let minimumDigitToCarGoForward = 4
-        let randomDigitNumberMaker = RandomDigitNumberMakerStub(desiredDigit: minimumDigitToCarGoForward)
+        randomDigitNumberMaker = RandomDigitNumberMakerStub(desiredDigit: minimumDigitToCarGoForward)
+    }
+    
+    override func tearDownWithError() throws {
+    }
+    
+    func test_roundCount_whenRaceCountIsZero_equalToZero() throws {
+        // given
+        let carCount = 0
+        let raceCount = 0
         sut = RacingCarGame(carCount: carCount,
                             raceCount: raceCount,
                             randomDigitNumberMaker: randomDigitNumberMaker)
-    }
-
-    override func tearDownWithError() throws {
-        sut = nil
-        carCount = nil
-        raceCount = nil
-    }
-    
-    func test_roundCount_equalReceCount() throws {
-        // given
         
         // when
         sut.start()
@@ -41,30 +36,88 @@ class RacingCarGameTests: XCTestCase {
         XCTAssertEqual(roundCount, raceCount)
     }
     
-    func test_carCountPerRound_equalCarCount() throws {
+    func test_roundCount_whenRaceCountIsOne_equalToOne() throws {
         // given
-        
+        let carCount = 0
+        let raceCount = 1
+        sut = RacingCarGame(carCount: carCount,
+                            raceCount: raceCount,
+                            randomDigitNumberMaker: randomDigitNumberMaker)
         // when
         sut.start()
         
+        // then
+        let roundCount = sut.roundHistory.rounds.count
+        XCTAssertEqual(roundCount, raceCount)
+    }
+    
+    func test_carCountPerRound_whenRoundIsZero_equalToNil() throws {
+        // given
+        let carCount = 1
+        let raceCount = 0
+        sut = RacingCarGame(carCount: carCount,
+                            raceCount: raceCount,
+                            randomDigitNumberMaker: randomDigitNumberMaker)
+
+        // when
+        sut.start()
+
+        // then
+        let firstRoundCarCount = sut.roundHistory.rounds.first?.cars.count
+        XCTAssertNil(firstRoundCarCount)
+    }
+    
+    func test_carCountPerRound_whenCarCountIsZero_equalToZero() throws {
+        // given
+        let carCount = 0
+        let raceCount = 1
+        sut = RacingCarGame(carCount: carCount,
+                            raceCount: raceCount,
+                            randomDigitNumberMaker: randomDigitNumberMaker)
+
+        // when
+        sut.start()
+
         // then
         let firstRoundCarCount = sut.roundHistory.rounds.first?.cars.count
         XCTAssertEqual(firstRoundCarCount, carCount)
     }
     
+
+    func test_carCountPerRound_whenCarCountIsOne_equalToOne() throws {
+        // given
+        let carCount = 1
+        let raceCount = 1
+        sut = RacingCarGame(carCount: carCount,
+                            raceCount: raceCount,
+                            randomDigitNumberMaker: randomDigitNumberMaker)
+
+        // when
+        sut.start()
+
+        // then
+        let firstRoundCarCount = sut.roundHistory.rounds.first?.cars.count
+        XCTAssertEqual(firstRoundCarCount, carCount)
+    }
+
     func test_carPosition_afterARace_forwardOneStep() throws {
         // given
-        
+        let carCount = 1
+        let raceCount = 2
+        sut = RacingCarGame(carCount: carCount,
+                            raceCount: raceCount,
+                            randomDigitNumberMaker: randomDigitNumberMaker)
+
         // when
         sut.start()
 
         // then
         let firstRound = sut.roundHistory.rounds[0]
         let positionInFirstRound = try XCTUnwrap(firstRound.cars.first).position
-        
+
         let secondRound = sut.roundHistory.rounds[1]
         let positionInSecondRound = try XCTUnwrap(secondRound.cars.first).position
-        
+
         XCTAssertEqual(positionInFirstRound + 1, positionInSecondRound)
     }
 }
