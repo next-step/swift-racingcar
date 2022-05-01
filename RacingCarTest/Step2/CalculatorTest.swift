55cac4c2b96d93336a55ccd4bc5b7febc352af57
이정배 <jeongbae1212@gmail.com>
2022-05-01 17:48:06 +0900

    test: 사칙연산 테스트 구현

--- RacingCarTest/Step2/CalculatorTest.swift
+++ RacingCarTest/Step2/CalculatorTest.swift
@@ -20,4 +20,34 @@ class CalculatorTest: XCTestCase {
         let calculator: Calculator = Calculator()
         try XCTAssertThrowsError(calculator.calculateStringExpression(input))
     }
+    
+    func testAdd() {
+        let calculator: Calculator = Calculator()
+
+        XCTAssertTrue(calculator.add(3, 5) == 8, "3 + 5 연산 결과가 8 아님")
+        XCTAssertTrue(calculator.add(0, -1) == -1, "0 + (-1) 연산 결과가 -1 아님")
+    }
+
+    func testSubtract() {
+        let calculator: Calculator = Calculator()
+
+        XCTAssertTrue(calculator.subtract(3, 5) == -2, "3 - 5 연산 결과가 -2 아님")
+        XCTAssertTrue(calculator.subtract(0, -1) == 1, "0 - (-1)  연산 결과가 1 아님")
+    }
+
+    func testMultiply() {
+        let calculator: Calculator = Calculator()
+
+        XCTAssertTrue(calculator.multiply(3, 5) == 15, "3 * 5 연산 결과가 15 아님")
+        XCTAssertTrue(calculator.multiply(0, -1) == 0, "0 * (-1)  연산 결과가 0 아님")
+    }
+
+    func testDivide() {
+        let calculator: Calculator = Calculator()
+
+        XCTAssertTrue(calculator.divide(3, 5) == 0, "3 / 5 연산 결과가 0 아님")
+        XCTAssertTrue(calculator.divide(0, -1) == 0, "0 / (-1)  연산 결과가 0 아님")
+        // TODO: 0으로 나눌 경우 fatal error 발생. CalculatorError 에 이 경우를 추가하는것도 좋아보인다.
+//        XCTAssertThrowsError(calculator.divide(5, 0))
+    }
 }
