be75992ca21df39832c41bd1587e012dc2c7046d
brianeunseokjeon <qed327@naver.com>
2022-05-01 14:08:57 +0900

    fix: Error print delete, Add number valid determine method

--- RacingCar/Step3/InputView.swift
+++ RacingCar/Step3/InputView.swift
@@ -11,19 +11,26 @@ struct InputView {
     func gameStart() throws -> (carCount:Int,attemptCount:Int) {
         GameSetting.carCount.showGuide()
         let carCount = try myReadLine()
+        try self.isValidNumber(carCount)
         
         GameSetting.attemptCount.showGuide()
         let attemptCount = try myReadLine()
+        try self.isValidNumber(attemptCount)
+        
         return (carCount,attemptCount)
     }
     
     private func myReadLine() throws -> Int {
         guard let input: String = readLine(), let inputNumber = Int(input) else {
-            let error = InputError.notNumber
-            error.showError()
-            throw error
+            throw InputError.notNumber
         }
         return inputNumber
+    }
+    
+    private func isValidNumber(_ input:Int) throws {
+        if input < 1 {
+            throw InputError.invalidNumber
+        }
     }
     
 }
