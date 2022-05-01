fd8f1ff14b2302c47b501383d8b22dce5fc2ebf4
brianeunseokjeon <qed327@naver.com>
2022-05-01 14:06:59 +0900

    feat: Make ResultView

--- RacingCar/Step3/ResultView.swift
+++ RacingCar/Step3/ResultView.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct ResultView {
+    func initSetting(_ attemptRound: Int) {
+        if attemptRound == 1 {
+            print("")
+            print("실행 결과")
+        }
+    }
+    
+    func result(car: Car) {
+        print(String(repeating: "-", count: car.distance))
+    }
+    
+}
