b2955846956d5ecd312f13081b20d51de5a11f54
csh8130 <99698759+csh8130@users.noreply.github.com>
2022-05-02 23:18:10 +0900

    step2 init

---
+++ RacingCar/Calculator.swift
@@ -0,0 +1,12 @@
+//
+//  Calculator.swift
+//  RacingCar
+//
+//  Created by main on 2022/05/02.
+//
+
+import Foundation
+
+class Calculator {
+    
+}
---
+++ RacingCarTest/CalculatorTests.swift
@@ -0,0 +1,23 @@
+//
+//  CalculatorTests.swift
+//  RacingCarTest
+//
+//  Created by main on 2022/05/02.
+//
+
+import XCTest
+
+class CalculatorTests: XCTestCase {
+    
+    override func setUpWithError() throws {}
+    
+    override func tearDownWithError() throws {}
+
+    func test_() throws {
+        // given
+        
+        // when
+        
+        // then
+    }
+}
