40059ed860309c31818020775f936bd5a2b8a047
brianeunseokjeon <qed327@naver.com>
2022-05-03 22:59:56 +0900

    fix

--- RacingCar/Step2/StringCalculatorError.swift
+++ RacingCar/Step2/StringCalculatorError.swift
@@ -10,15 +10,9 @@ import Foundation
 enum StringCalculatorError: Error {
     case inputNil
     case inputEmpty
-<<<<<<< HEAD
     case isNotOperator
     case number
     case calculator
-=======
-    case operatorError
-    case numberError
-    case calculatorError
->>>>>>> a694801 (feat: Make stringCalculator)
     
     
     var errorDescription: String {
@@ -27,19 +21,11 @@ enum StringCalculatorError: Error {
             return "입력값이 nil 입니다."
         case .inputEmpty:
             return "입력값이 빈 값입니다."
-<<<<<<< HEAD
         case .isNotOperator:
             return "사칙연산이 아닙니다"
         case .number:
             return "숫자가 아닙니다"
         case .calculator:
-=======
-        case .operatorError:
-            return "사칙연산이 아닙니다"
-        case .numberError:
-            return "숫자가 아닙니다"
-        case .calculatorError:
->>>>>>> a694801 (feat: Make stringCalculator)
             return "계산 불가합니다"
         }
     }
