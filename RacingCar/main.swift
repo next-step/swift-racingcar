a15ab10a913765be4b4f143c4fea5882ff67e37f
brianeunseokjeon <qed327@naver.com>
2022-05-03 23:01:22 +0900

    fix

--- RacingCar/main.swift
+++ RacingCar/main.swift
@@ -6,18 +6,5 @@
 
 import Foundation
 
-<<<<<<< HEAD
 let game = GameController()
 game.gameStart()
-=======
-//let s = StringCalculator()
-//
-//do {
-//    print(try s.caculate(input: "1 + 5 / 3"))
-//
-//} catch {
-//    if let error = error as? StringCalculatorError {
-//        print(error.errorDescription)
-//    }
-//}
->>>>>>> a694801 (feat: Make stringCalculator)
