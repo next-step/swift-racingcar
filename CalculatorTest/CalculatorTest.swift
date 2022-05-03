13360290d9d93a91c965a2e33623fef5ba3fa192
woosub-lee <woosub.dev@gmail.com>
2022-05-03 12:37:59 +0900

    test: remove unuse code

--- CalculatorTest/CalculatorTest.swift
+++ CalculatorTest/CalculatorTest.swift
@@ -8,14 +8,6 @@
 import XCTest
 
 class CalculatorTest: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
     
     func test_1_과_2를_덧셈() {
         let sut = Calculator(for: "1 + 2")
