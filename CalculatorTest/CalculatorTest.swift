4b5bf134584958479c2a9e5258368c9d18b872e9
woosub-lee <woosub.dev@gmail.com>
2022-05-03 12:46:23 +0900

    test: migrate for setup method

--- CalculatorTest/CalculatorTest.swift
+++ CalculatorTest/CalculatorTest.swift
@@ -10,72 +10,86 @@ import XCTest
 class CalculatorTest: XCTestCase {
     
     func test_1_과_2를_덧셈() {
-        let sut = Calculator(for: "1 + 2")
+        let sut = Calculator()
+        sut.setup(formula: "1 + 2")
         XCTAssertEqual(try sut.calculate(), 3)
     }
     
     func test_1_과_2를_뺄셈() {
-        let sut = Calculator(for: "1 - 2")
+        let sut = Calculator()
+        sut.setup(formula: "1 - 2")
         XCTAssertEqual(try sut.calculate(), -1)
     }
     
     func test_2_와_3을_곱셈() {
-        let sut = Calculator(for: "2 * 3")
+        let sut = Calculator()
+        sut.setup(formula: "2 * 3")
         XCTAssertEqual(try sut.calculate(), 6)
     }
 
     func test_6_과_2를_나눗셈() {
-        let sut = Calculator(for: "6 / 2")
+        let sut = Calculator()
+        sut.setup(formula: "6 / 2")
         XCTAssertEqual(try sut.calculate(), 3)
     }
     
     func test_나머지가_생기는_나눗셈() {
-        let sut = Calculator(for: "5 / 2")
+        let sut = Calculator()
+        sut.setup(formula: "5 / 2")
         XCTAssertEqual(try sut.calculate(), 2)
     }
     
     func test_사칙연산이_모두_존재하는_수식() {
-        let sut = Calculator(for: "2 + 3 * 4 / 2")
+        let sut = Calculator()
+        sut.setup(formula: "2 + 3 * 4 / 2")
         XCTAssertEqual(try sut.calculate(), 10)
     }
     
     func test_올바르지_못한_연산자가_존재하는_수식() {
-        let sut = Calculator(for: "1 @ 2")
+        let sut = Calculator()
+        sut.setup(formula: "1 @ 2")
         XCTAssertThrowsError(try sut.calculate())
     }
     
     func test_연산자가_없는_수식() {
-        let sut = Calculator(for: "1 2")
+        let sut = Calculator()
+        sut.setup(formula: "1 2")
         XCTAssertThrowsError(try sut.calculate())
     }
     
     func test_숫자가_하나_부족한_수식() {
-        let sut = Calculator(for: "1 + ")
+        let sut = Calculator()
+        sut.setup(formula: "1 + ")
         XCTAssertThrowsError(try sut.calculate())
     }
     
     func test_연산자만_존재하는_수식() {
-        let sut = Calculator(for: " + - ")
+        let sut = Calculator()
+        sut.setup(formula: " + - ")
         XCTAssertThrowsError(try sut.calculate())
     }
     
     func test_빈_문자열이_주어진_수식() {
-        let sut = Calculator(for: "")
+        let sut = Calculator()
+        sut.setup(formula: "")
         XCTAssertThrowsError(try sut.calculate())
     }
     
     func test_빈_공백_문자열이_주어진_수식() {
-        let sut = Calculator(for: " ")
+        let sut = Calculator()
+        sut.setup(formula: " ")
         XCTAssertThrowsError(try sut.calculate())
     }
     
     func test_숫자_하나만_주어진_수식() {
-        let sut = Calculator(for: "1")
+        let sut = Calculator()
+        sut.setup(formula: "1")
         XCTAssertEqual(try sut.calculate(), 1)
     }
     
     func test_숫자_여러개만_주어진_수식() {
-        let sut = Calculator(for: "1 2 3")
+        let sut = Calculator()
+        sut.setup(formula: "1 2 3")
         XCTAssertThrowsError(try sut.calculate())
     }
 }
