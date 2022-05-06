1d53bef5002a5b1a1fda46e990446264f55ccf13
brianeunseokjeon <qed327@naver.com>
2022-05-06 22:19:20 +0900

    안쓰는 메서드 정리

--- RacingCar/Step3/View/InputView.swift
+++ RacingCar/Step3/View/InputView.swift
@@ -8,18 +8,6 @@
 import Foundation
 
 struct InputView {
-    func gameStart() throws -> (carCount:Int,attemptCount:Int) {
-        print(GameGuide.carCount, terminator: " ")
-        let carCount = try readInput()
-        try self.isValidNumber(carCount)
-        
-        print(GameGuide.attemptCount, terminator: " ")
-        let attemptCount = try readInput()
-        try self.isValidNumber(attemptCount)
-        
-        return (carCount,attemptCount)
-    }
-    
     func carNamesSetting() -> String {
         print(GameGuide.carNames)
         guard let input: String = readLine() else {
