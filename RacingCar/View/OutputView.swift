d947c5fce3370ef71d6b947c9e31b0eddff03341
RoadtoS7 <kshvv975@gmail.com>
2022-05-07 13:47:12 +0900

    refactor: 사용하지 않는 OutputView 메서드 제거

--- RacingCar/View/OutputView.swift
+++ RacingCar/View/OutputView.swift
@@ -17,18 +17,6 @@ struct OutputView {
         print("\(winners)가 최종 우승했습니다.")
     }
     
-    static func carCountIsInvalid() {
-        print("자동차 대수는 양의 정수여야 합니다.")
-    }
-    
-    static func roundCountIsInvalid() {
-        print("시도할 횟수는 양의 정수여야 합니다.")
-    }
-    
-    static func carNameIsLongerThan(validLength: Int, invalidName: String) {
-        print("자동차 이름은 \(validLength)이하여야 합니다. 잘못된 이름: \(invalidName)")
-    }
-    
     static func printError(_ error: Error) {
         print(error.localizedDescription)
     }
