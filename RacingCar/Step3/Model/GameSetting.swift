db8a38390cb75d80e88544c51cb99908b27777f6
brianeunseokjeon <qed327@naver.com>
2022-05-08 17:52:30 +0900

    feat: 게임세팅 객체 생성

---
+++ RacingCar/Step3/Model/GameSetting.swift
@@ -0,0 +1,13 @@
+//
+//  GameSetting.swift
+//  RacingCar
+//
+//  Created by brian은석 on 2022/05/08.
+//
+
+import Foundation
+
+struct GameSetting {
+    let carNames: [String]
+    let attemptCount: Int
+}
