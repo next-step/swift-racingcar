48cd1afabf8fbe9f84f67da2b6fee8a32646274d
hebaek76916 <qorgusdms15@gmail.com>
2022-05-16 02:55:12 +0900

    Delete unnecessaries.

--- RacingCarTest/RacingCarTest.swift
+++ RacingCarTest/RacingCarTest.swift
@@ -10,22 +10,6 @@ import XCTest
 
 class RacingCarTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
     func test2더하기3은5() throws {
         let calculator: Calculator = Calculator()
         XCTAssert(calculator.add(2,3) == 5, "2 + 3 = 5 실패")
@@ -51,13 +35,6 @@ class RacingCarTest: XCTestCase {
         XCTAssert(calculator.divide(10,5) == 2, "10 / 5 = 2 실패")
         XCTAssertTrue(calculator.divide(10,5) == 2, "10 / 5 = 2 실패")
         XCTAssertEqual(calculator.divide(10,5), 2, "10 / 5 = 2 실패")
-    }
-    
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
