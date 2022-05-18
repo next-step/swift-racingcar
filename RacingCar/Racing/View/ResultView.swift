4a5a71f565e26c3dc12d388620ce7099b89c411a
KimDuni <7577ksj@gmail.com>
2022-05-18 17:39:09 +0900

    feat: 경주에서 우승한 자동차명을 출력하는 메서드 추가

--- RacingCar/Racing/View/ResultView.swift
+++ RacingCar/Racing/View/ResultView.swift
@@ -10,8 +10,25 @@ import Foundation
 struct ResultView {
     func printCars(participatedCars: [Car]) {
         participatedCars.forEach({ car in
-            print(String(repeating: "-", count: car.moveDistance))
+            print("\(car.name) : " + String(repeating: "-", count: car.moveDistance))
         })
         print("\n\n")
+    }
+    
+    func printWinner(participatedCars: [Car]) {
+        let topScore = racingTopScore(participatedCars)
+        let winners = participatedCars
+            .filter { $0.moveDistance == topScore }
+            .compactMap { $0.name }
+            .joined(separator: ", ")
+        
+        print("\(winners)가 최종 우승했습니다.")
+    }
+    
+    private func racingTopScore(_ cars: [Car]) -> Int {
+        if let topScore = cars.compactMap({ $0.moveDistance }).max() {
+            return topScore
+        }
+        return 0
     }
 }
