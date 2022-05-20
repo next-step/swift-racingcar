8c81cfbe56ed337d2a64f85b70fdd58966fc3264
KimDuni <7577ksj@gmail.com>
2022-05-20 11:01:06 +0900

    refactor: 결과뷰에서는 계산 로직 없이 전달받은 자동차 정보로만 각 메서드에 맞는 출력문구를 생성하여 print하도록 함

--- RacingCar/Racing/View/ResultView.swift
+++ RacingCar/Racing/View/ResultView.swift
@@ -8,27 +8,18 @@
 import Foundation
 
 struct ResultView {
-    func printCars(participatedCars: [Car]) {
+    func printProgress(_ participatedCars: [Car]) {
         participatedCars.forEach({ car in
             print("\(car.name) : " + String(repeating: "-", count: car.moveDistance))
         })
         print("\n\n")
     }
     
-    func printWinner(participatedCars: [Car]) {
-        let topScore = racingTopScore(participatedCars)
-        let winners = participatedCars
-            .filter { $0.moveDistance == topScore }
+    func printGameResult(winnerCars: [Car]) {
+        let topScoreCar = winnerCars
             .compactMap { $0.name }
             .joined(separator: ", ")
         
-        print("\(winners)가 최종 우승했습니다.")
-    }
-    
-    private func racingTopScore(_ cars: [Car]) -> Int {
-        if let topScore = cars.compactMap({ $0.moveDistance }).max() {
-            return topScore
-        }
-        return 0
+        print("\(topScoreCar)가 최종 우승했습니다.")
     }
 }
